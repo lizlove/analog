3e9b361111d9bcf2eb0e7fd1da9c53a07e9504d2
lizlove <elizabethlovero@gmail.com>
2015-06-20 12:14:29 -0400

    first commit

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+# Gems
+require 'open-uri'
+require 'nokogiri'
+require 'bundler'
+Bundler.require
+
+# Libraries
+require_relative '../app/models/analog'
+require_relative '../app/models/analog_scraper'
+require_relative '../app/controllers/analog_cli'
